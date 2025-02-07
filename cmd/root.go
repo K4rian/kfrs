0aec37efed8439436ebdaefc88f5c52e4f811b32
K4rian <contact@k4rian.com>
2025-02-07 19:26:03 +0000

    Add arguments validation...
    - Move config values assignment to initConfig()
    - Moved validation of host, port, directory, max-requests, and ban-time into validateConfig()
    - Improved error handling in getConfigValue()

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"log"
+	"net"
 	"os"
 	"strconv"
 	"strings"
@@ -31,15 +32,7 @@ func BuildRootCommand() *cobra.Command {
 		Use:   "kfrs",
 		Short: "KF HTTP Redirect Server",
 		Long:  "A command-line tool to run an HTTP Redirect Server used by the Killing Floor Dedicated Server (kfds).",
-		Run: func(cmd *cobra.Command, args []string) {
-			Host = getConfigValue(cmd, "host", defaultHost).(string)
-			Port = getConfigValue(cmd, "port", defaultPort).(int)
-			Directory = getConfigValue(cmd, "directory", defaultDirectory).(string)
-			MaxRequests = getConfigValue(cmd, "max-requests", defaultMaxRequests).(int)
-			BanTime = getConfigValue(cmd, "ban-time", defaultBanTime).(int)
-
-			printFlags()
-		},
+		Run:   runRootCommand,
 	}
 
 	helpFunc := rootCmd.HelpFunc()
@@ -78,12 +71,37 @@ func BuildRootCommand() *cobra.Command {
 	return rootCmd
 }
 
+func runRootCommand(cmd *cobra.Command, args []string) {
+	// Init configuration values
+	initConfig(cmd)
+
+	// Validate config values
+	validateConfig()
+
+	// Print the final configuration
+	printConfig()
+}
+
+func initConfig(cmd *cobra.Command) {
+	Host = getConfigValue(cmd, "host", defaultHost).(string)
+	Port = getConfigValue(cmd, "port", defaultPort).(int)
+	Directory = getConfigValue(cmd, "directory", defaultDirectory).(string)
+	MaxRequests = getConfigValue(cmd, "max-requests", defaultMaxRequests).(int)
+	BanTime = getConfigValue(cmd, "ban-time", defaultBanTime).(int)
+}
+
 func getConfigValue(cmd *cobra.Command, key string, defaultValue any) any {
+	switch defaultValue.(type) {
+	case int:
+	case string:
+	default:
+		log.Fatalf("Unsupported type for flag %s: %T", key, defaultValue)
+	}
+
 	// CLI arguments take precedence over environment variables
 	if cmd.Flags().Changed(key) {
 		val := cmd.Flag(key).Value.String()
-
-		switch v := defaultValue.(type) {
+		switch defaultValue.(type) {
 		case int:
 			intVal, err := strconv.Atoi(val)
 			if err != nil {
@@ -92,25 +110,51 @@ func getConfigValue(cmd *cobra.Command, key string, defaultValue any) any {
 			return intVal
 		case string:
 			return val
-		default:
-			log.Fatalf("Unsupported type for flag %s: %T", key, v)
 		}
 	}
 
 	if viper.IsSet(key) {
-		switch v := defaultValue.(type) {
+		switch defaultValue.(type) {
 		case int:
 			return viper.GetInt(key)
 		case string:
 			return viper.GetString(key)
-		default:
-			log.Fatalf("Unsupported type for flag %s: %T", key, v)
 		}
 	}
 	return defaultValue
 }
 
-func printFlags() {
+func validateConfig() {
+	// Host validation (v4, v6, or domain)
+	if net.ParseIP(Host) == nil {
+		_, err := net.LookupHost(Host)
+		if err != nil {
+			log.Fatalf("Invalid host: %s", Host)
+		}
+	}
+
+	// Port validation
+	if Port <= 1024 || Port >= 65535 {
+		log.Fatalf("Port must be between 1024 and 65535: %d", Port)
+	}
+
+	// The directory must exist
+	if _, err := os.Stat(Directory); os.IsNotExist(err) {
+		log.Fatalf("Directory does not exist: %s", Directory)
+	}
+
+	// Max Requests validation (> 0 and < 100)
+	if MaxRequests <= 0 || MaxRequests >= 100 {
+		log.Fatalf("Max requests should be between 1 and 99: %d", MaxRequests)
+	}
+
+	// Ban Time validation (should be > 0)
+	if BanTime <= 0 {
+		log.Fatalf("Ban time should be greater than 0: %d", BanTime)
+	}
+}
+
+func printConfig() {
 	log.Println("===================================================")
 	log.Println("                   KFRS Settings                   ")
 	log.Println("===================================================")
