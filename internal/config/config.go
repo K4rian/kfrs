9444968e3760c6f196a6e519841b29cde6c5b9f1
K4rian <contact@k4rian.com>
2025-02-10 09:29:27 +0000

    Fix Config struct validation method

--- internal/config/config.go
+++ internal/config/config.go
@@ -25,58 +25,58 @@ type Config struct {
 
 func (c *Config) Validate() error {
 	// Host validation (v4, v6, or domain)
-	if net.ParseIP(conf.Host) == nil {
-		_, err := net.LookupHost(conf.Host)
+	if net.ParseIP(c.Host) == nil {
+		_, err := net.LookupHost(c.Host)
 		if err != nil {
-			return fmt.Errorf("invalid host: %s", conf.Host)
+			return fmt.Errorf("invalid host: %s", c.Host)
 		}
 	}
 
 	// Port validation
-	if conf.Port <= 1024 || conf.Port >= 65535 {
-		return fmt.Errorf("port must be between 1025 and 65534: %d", conf.Port)
+	if c.Port <= 1024 || c.Port >= 65535 {
+		return fmt.Errorf("port must be between 1025 and 65534: %d", c.Port)
 	}
 
 	// The directory must exist
-	if _, err := os.Stat(conf.Directory); os.IsNotExist(err) {
-		return fmt.Errorf("directory does not exist: %s", conf.Directory)
+	if _, err := os.Stat(c.Directory); os.IsNotExist(err) {
+		return fmt.Errorf("directory does not exist: %s", c.Directory)
 	}
 
 	// Max Requests validation (> 0 and < 100)
-	if conf.MaxRequests <= 0 || conf.MaxRequests >= 100 {
-		return fmt.Errorf("max requests should be between 1 and 99: %d", conf.MaxRequests)
+	if c.MaxRequests <= 0 || c.MaxRequests >= 100 {
+		return fmt.Errorf("max requests should be between 1 and 99: %d", c.MaxRequests)
 	}
 
 	// Ban Time validation (should be > 0)
-	if conf.BanTime <= 0 {
-		return fmt.Errorf("ban time should be greater than 0: %d", conf.BanTime)
+	if c.BanTime <= 0 {
+		return fmt.Errorf("ban time should be greater than 0: %d", c.BanTime)
 	}
 
 	// Log level
 	validLevels := []string{"info", "debug", "warn", "error"}
-	if !slices.Contains(validLevels, conf.LogLevel) {
-		return fmt.Errorf("invalid log level: %s", conf.LogLevel)
+	if !slices.Contains(validLevels, c.LogLevel) {
+		return fmt.Errorf("invalid log level: %s", c.LogLevel)
 	}
 
 	// Log file format (text or json)
 	validFormat := []string{"text", "json"}
-	if !slices.Contains(validFormat, conf.LogFileFormat) {
-		return fmt.Errorf("invalid log file format: %s", conf.LogFileFormat)
+	if !slices.Contains(validFormat, c.LogFileFormat) {
+		return fmt.Errorf("invalid log file format: %s", c.LogFileFormat)
 	}
 
 	// Log max size (should be > 0)
-	if conf.LogMaxSize < 1 {
-		return fmt.Errorf("log max size should be greater than 0: %d", conf.LogMaxSize)
+	if c.LogMaxSize < 1 {
+		return fmt.Errorf("log max size should be greater than 0: %d", c.LogMaxSize)
 	}
 
 	// Log max backups (should be > 0)
-	if conf.LogMaxBackups < 1 {
-		return fmt.Errorf("log max backups should be greater than 0: %d", conf.LogMaxBackups)
+	if c.LogMaxBackups < 1 {
+		return fmt.Errorf("log max backups should be greater than 0: %d", c.LogMaxBackups)
 	}
 
 	// Log max age (should be > 0)
-	if conf.LogMaxAge < 1 {
-		return fmt.Errorf("log max backups should be greater than 0: %d", conf.LogMaxAge)
+	if c.LogMaxAge < 1 {
+		return fmt.Errorf("log max backups should be greater than 0: %d", c.LogMaxAge)
 	}
 	return nil
 }
